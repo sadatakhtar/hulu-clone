5b936b6b2600c3695e3b1d432e831096fbeb1496
sadatakhtar <sadat0akhtar@gmail.com>
2021-07-27 17:09:15 +0100

    creates header with headerItem, still not complete yet

---
+++ components/HeaderItem.js
@@ -0,0 +1,12 @@
+function HeaderItem({ title, Icon }) {
+  return (
+    <div className='flex group flex-col items-center cursor-pointer w-12
+    sm:w-20 hover:text-white '>
+      <Icon className='h-8 mb-1 hover:animate-bounce' />
+      <p className=' opacity-0 tracking-widest group-hover:opacity-100'>{title}</p>
+      
+    </div>
+  )
+}
+
+export default HeaderItem

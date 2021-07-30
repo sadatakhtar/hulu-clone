c22f8f5ff0b830d7f1e96b167de90dbde7d32071
sadatakhtar <sadat0akhtar@gmail.com>
2021-07-30 21:56:56 +0100

    changed Thumbnails.js component

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { ThumbUpIcon } from '@heroicons/react/outline'
 import { forwardRef } from 'react'
 
-const Thumbnail = forwardRef(({ result}, ref) => {
+export const Thumbnail = forwardRef(({ result}, ref) => {
 
  // const BASE_URL = `https://image.tmdb.org/t/p/original/`;
   const BASE_URL = `https://image.tmdb.org/t/p/w500/`;
